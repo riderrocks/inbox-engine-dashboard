39a04ae6af2b2f4eaa4d98bb16d2c994ae26327a
ananth.raghav <ananth.raghav@bookmyshow.com>
2016-12-20 14:10:50 +0530

    timetick and routeName added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var inboxUrl = 'http://172.16.65.7:6633';
 
-var app = angular.module('myApp', ['ngRoute', 'myApp.header', 'myApp.dashboard', 'myApp.new', 'myApp.scheduled', 'myApp.sent', 'myApp.listAnnouncements', 'myApp.listNotifications', 'myApp.announcement', 'myApp.notification']).config(['$routeProvider', function($routeProvider) {
+var app = angular.module('myApp', ['ngRoute', 'myApp.dashboard', 'myApp.new', 'myApp.scheduled', 'myApp.sent', 'myApp.listAnnouncements', 'myApp.listNotifications', 'myApp.announcement', 'myApp.notification']).config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({
         redirectTo: '/dashboard'
     });
--- controllers/header.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('myApp.header', ['ngRoute']).controller('NavbarCtrl', ['$scope', '$location', function($scope, $location) {
-    $scope.isRouteActive = function(route) {
-        var curRoute = $location.path();
-        return curRoute.match(route);
-    }
-}]);
