b2442ebc052dc57d043d5e0e93c1966a08c30e74
ananth.raghav <ananth.raghav@bookmyshow.com>
2016-12-27 11:57:05 +0530

    campaign messages countupdated

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var inboxUrl = 'http://172.16.66.54:6633';
+var inboxUrl = 'http://172.16.65.7:6633';
 
-var app = angular.module('myApp', ['ngRoute', 'myApp.dashboard', 'myApp.new', 'myApp.scheduled', 'myApp.sent', 'myApp.listAnnouncements', 'myApp.listNotifications', 'myApp.announcement', 'myApp.notification']).config(['$routeProvider', function($routeProvider) {
+var app = angular.module('myApp', ['ngRoute', 'myApp.dashboard', 'myApp.new', 'myApp.scheduled', 'myApp.sent', 'myApp.listAnnouncements', 'myApp.listNotifications', 'myApp.announcement', 'myApp.notification', 'myApp.viewCampaigns']).config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({
         redirectTo: '/dashboard'
     });
