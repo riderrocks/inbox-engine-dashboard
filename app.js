2d2954722f445ca0123ac9d96b41ce0ee555cf4e
riderrocks <ananth.raghav@bookmyshow.com>
2016-12-30 15:08:08 +0530

    default route changed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var inboxUrl = 'http://172.16.66.81:6633';
+var inboxUrl = 'http://172.16.65.7:6633';
 
 var app = angular.module('myApp', ['ngRoute', 'myApp.dashboard', 'myApp.new', 'myApp.sent', 'myApp.listAnnouncements', 'myApp.listNotifications', 'myApp.announcement', 'myApp.notification', 'myApp.viewCampaigns']).config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({
-        redirectTo: '/dashboard'
+        redirectTo: '/viewCampaigns'
     });
 }]).constant('CONFIG', {
     INBOX: {
