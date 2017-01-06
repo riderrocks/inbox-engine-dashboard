d8119cf7a4a181174fb75a8cec2a2acd3cfe3ce1
riderrocks <ananth.raghav@bookmyshow.com>
2017-01-06 19:21:25 +0530

    Custom key value functionality done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var inboxUrl = 'http://172.16.66.54:6633';
+var inboxUrl = 'http://172.16.65.7:6633';
 
 var app = angular.module('myApp', ['ngRoute', 'myApp.dashboard', 'myApp.createCampaign', 'myApp.sent', 'myApp.listAnnouncements', 'myApp.listNotifications', 'myApp.editAnnouncement', 'myApp.editNotification', 'myApp.viewCampaigns']).config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({
