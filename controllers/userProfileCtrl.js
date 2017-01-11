e10e1613d82fb411d6104a628f8e838e0bb7509b
Bhargava <bhargava.s@bookmyshow.com>
2017-01-11 16:11:03 +0530

    profile creation

---
+++ controllers/userProfileCtrl.js
@@ -0,0 +1,49 @@
+'use strict';
+angular.module('myApp.userProfile', ['ngRoute']).config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/profile', {
+        templateUrl: 'views/profile.html',
+    });
+}]).controller('ProfileCtrl', ['$scope', 'MessageService', 'AuthenticationService', function($scope, MessageService, AuthenticationService) {
+
+    $scope.user = {};
+    $scope.alertFlag = false;
+    $scope.alertMsg = '';
+
+    $scope.validatePassword = function(user) {
+        if ($scope.user.newPassword == null || $scope.user.confirmPassword == null || $scope.user.oldPassword == null) {
+            $scope.alertFlag = true;
+            $scope.alertMsg = 'Oops! Fill all mandatory fields please';
+            return;
+        }
+        var result = angular.equals($scope.user.newPassword, $scope.user.confirmPassword);
+        if (!result) {
+            $scope.alertFlag = true;
+            $scope.alertMsg = 'Confirm password should be same as new password';
+        } else {
+            $scope.alertFlag = false;
+            $scope.alertMsg = '';
+        }
+
+    };
+    $scope.resetPassword = function(user) {
+        if ($scope.user.newPassword == null || $scope.user.confirmPassword == null || $scope.user.oldPassword == null) {
+            $scope.alertFlag = true;
+            $scope.alertMsg = 'Oops! Fill all mandatory fields please';
+            return;
+        }
+        $scope.validatePassword(user);
+
+        if (!$scope.alertFlag) {
+            var token = AuthenticationService.getToken();
+            AuthenticationService.resetPassword(user, token).then(function(response) {
+                if (response.data.status.httpResponseCode == 200) {
+                    $scope.user = {};
+                    swal("Done!", "Password updated successfully", "success");
+                }
+
+            }).catch(function(response){
+                    swal("Oops!", "Old password is incorrect", "error");
+            });
+        }
+    };
+}]);
